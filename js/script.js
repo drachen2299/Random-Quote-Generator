d1a9494f0c35410b8798699ced10d8edd88907b8
Ben <39630191+drachen2299@users.noreply.github.com>
2020-03-22 13:13:33 -0600

    added let vars

--- js/script.js
+++ js/script.js
@@ -10,12 +10,31 @@ project 1 - A Random Quote Generator
 /***
  * `quotes` array
 ***/
-let quotes = [
+const quotes = [
   //Winston Churchill Quote
   {
     quote: "There will come a time when you believe everything is finished. That will be the beginning.",
-    author: "Louis L'Amour, Unkown, 2019",
+    source: "Louis L'Amour, Unkown, 2019",
   },
+  {
+    quote: "And the piano, it sounds like a carnival, and the microphone smells like a beer, \
+    And they sit at the bar, and put bread in my jar, and say man what are you doing here",
+    source: "Billy Joel, Piano Man, 1973",
+  },
+  {
+    quote: "Be yourself, everyone else is already taken.",
+    source: "Oscar Wilde, The Hudson Review, 1967",
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    source: "Thomas A. Edison, Unknown, Circa 1910",
+  },
+  {
+    quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.\
+     You're on your own. And you know what you know. And YOU are the one who'll decide where to go...",
+    source: "Dr Seuss, Oh, The Places You'll Go, 1990",
+  }
+
 ];
 
 
@@ -33,8 +52,8 @@ function getRandomQuote() {
  * `printQuote` function
 ***/
 function printQuote(message){
-  let result = getRandomQuote(); //Calls and stores the getRandomQuote is a variable
-  let message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.author + "</p>";
+  var result = getRandomQuote(); //Calls and stores the getRandomQuote is a variable
+  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.source + "</p>";
    document.getElementById('quote-box').innerHTML = message;
 }
 
