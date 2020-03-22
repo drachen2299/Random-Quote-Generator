96af023c0f5bb0779033683e77e9d27ceb94e800
Ben <39630191+drachen2299@users.noreply.github.com>
2020-03-21 20:57:49 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,25 +3,40 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+let quotes = [
+  //Winston Churchill Quote
+  {
+    quote: "There will come a time when you believe everything is finished. That will be the beginning.",
+    author: "Louis L'Amour, Unkown, 2019",    
+  },
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+// getRandomQuote function selects and returns a random quote object
+function getRandomQuote() {
+    var quoteIndex = Math.floor(Math.random() * quotes.length);  // selects a random quote
+    return quotes[quoteIndex];  // returns the random quote
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote(message){
+  var result = getRandomQuote(); //Calls and stores the getRandomQuote is a variable
+  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.author + "</p>";
+   document.getElementById('quote-box').innerHTML = message;
+}
 
 
 
