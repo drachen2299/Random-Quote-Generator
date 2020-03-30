c861250df98f80ff92de72cefb4a108e09029c26
Ben <39630191+drachen2299@users.noreply.github.com>
2020-03-30 15:58:01 -0600

    added an if statment to not repeat quotes

--- js/script.js
+++ js/script.js
@@ -10,29 +10,39 @@ project 1 - A Random Quote Generator
 /***
  * `quotes` array
 ***/
-const quotes = [
+let quotes = [
   //Winston Churchill Quote
   {
     quote: "There will come a time when you believe everything is finished. That will be the beginning.",
-    source: "Louis L'Amour, Unkown, 2019",
+    citation:"Unkown",
+    source: "Louis L'Amour",
+    year:"2019"
   },
   {
     quote: "And the piano, it sounds like a carnival, and the microphone smells like a beer, \
     And they sit at the bar, and put bread in my jar, and say man what are you doing here",
-    source: "Billy Joel, Piano Man, 1973",
+    citation:"Piano Man",
+    source: "Billy Joel",
+    year:"1973"
   },
   {
     quote: "Be yourself, everyone else is already taken.",
-    source: "Oscar Wilde, The Hudson Review, 1967",
+    citation:" The Hudson Review",
+    source: "Oscar Wilde",
+    year:"1967"
   },
   {
     quote: "I have not failed. I've just found 10,000 ways that won't work.",
-    source: "Thomas A. Edison, Unknown, Circa 1910",
+    citation:"Unknown",
+    source: "Thomas A. Edison",
+    year:"Circa 1910"
   },
   {
     quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.\
      You're on your own. And you know what you know. And YOU are the one who'll decide where to go...",
-    source: "Dr Seuss, Oh, The Places You'll Go, 1990",
+    citation:"Oh, The Places You'll Go",
+    source: "Dr Seuss",
+    year:"1990"
   }
 
 ];
@@ -41,11 +51,26 @@ const quotes = [
 /***
  * `getRandomQuote` function
 ***/
+// variable that will hold viewed quotes
+let viewedQuotes = [];
 // getRandomQuote function selects and returns a random quote object
-function getRandomQuote() {
-    let quoteIndex = Math.floor(Math.random() * quotes.length);  // selects a random quote from the array
-    return quotes[quoteIndex];  // returns the random quote
+function getRandomQuote(){
+  if (quotes.length === 0){
+    quotes = viewedQuotes.splice(0, viewedQuotes.length);
+  }
+  let randNum = Math.floor(Math.random() * quotes.length);
+  let splicedQuote = quotes.splice(randNum, 1)[0];
+  viewedQuotes.push(splicedQuote);
+  return splicedQuote;
 }
+/*function getRandomQuote() {
+    let quoteIndex = Math.floor(Math.random() * quotes.length);  // selects a random quote from the array
+    if (quotes.length === 0) {
+      quotes.push(viewedQuotes);
+      viewedQuotes = [0];
+    }
+    return quotes[quoteIndex];  // returns the random quote
+}*/
 
 
 /***
@@ -53,7 +78,8 @@ function getRandomQuote() {
 ***/
 function printQuote(message){
   var result = getRandomQuote(); //Calls and stores the getRandomQuote is a variable
-  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.source + "</p>";
+  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='citation'>" + result.citation + "</p>"+
+  "<p class='source'>" + result.source + "</p>"+"<p class='source'>" + result.year + "</p>";
    document.getElementById('quote-box').innerHTML = message;
 }
 
